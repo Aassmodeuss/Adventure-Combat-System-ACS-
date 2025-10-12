eaf4d2003aabd8779363de5c8ba3c2fa0c384f1e
Aassmodeuss <221757530+Aassmodeuss@users.noreply.github.com>
2025-10-12 15:07:30 -0700

    Delete Heart of the Forest directory

--- Heart of the Forest/Legacy Archives/AutoCards/src/context.js
+++
@@ -1,9 +0,0 @@
-// Your "Context" tab should look like this
-const modifier = (text) => {
-  // Your other context modifier scripts go here (preferred)
-  [text, stop] = AutoCards("context", text, stop);
-  text = LocalizedLanguages("context", text);
-  // Your other context modifier scripts go here (risky)
-  return { text, stop };
-};
-modifier(text);
